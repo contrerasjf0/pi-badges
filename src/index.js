ec93735eca45b8066a2b9048fd36ffd5a1406bf9
Juan Francisco <contrerasjf.0@hotmail.com>
2019-11-02 10:53:30 -0600

    feature(router): Implement the router handle with react-router
    * React-router-dom library wa sinstalled.
    * React-router-dom was configurated in the App component.
    * Add the Link component to go badge-form in the Badges component .
    
    Resolve: C-19

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import BadgeNew from './pages/BadgeNew';
-import Badges from './pages/Badges';
+import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -12,7 +11,7 @@ import './statics/scss/global.scss';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Badges /> , document.getElementById('root'));
+ReactDOM.render(<App /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
