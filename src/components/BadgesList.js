0af281ddf43ff37db0fa82d100f739cae47f9d74
Juan Francisco <contrerasjf.0@hotmail.com>
2019-11-03 22:55:52 -0600

    feature(service): Implement the services layer
    * A service file was creted to request the data to the server.
    * A JSON file was created with fake data with the seeder.
    * A factory of fake data was created.
    * New libraries(faker, json-server and npm-run-all)  were installed to implement a fake json server
    * The scripst were update to run the json server.
    * The BadgesList and Badges were updated to do the data request and to show messages when the data is requested, have an error or there aren´t data.
    
    Resolve: C-25

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import BadgeItem from './BadgeItem';
+
+
 
 class BadgesList extends Component {
     render() {
+
+        if (this.props.badges.length === 0) {
+                return (
+                <div>
+                    <h3>No badges were found</h3>
+                    <Link className="btn btn-primary" to="/badges/new">
+                    Create new badge
+                    </Link>
+                </div>
+                );
+        }
         return (
         <div className="BadgesList">
             <ul className="list-unstyled">
