4fc8d1bafe5089bce851d6d64f056b38b5debe73
Juan Francisco <contrerasjf.0@hotmail.com>
2019-11-24 09:25:32 -0600

    feature(Search filter): Implement the search filter in BadgesList Component
    * The BadgesList componet's structure was change of class to function.
    * The Search Filter logic was implemented with hook and custome hook.
    
    Resolve: C-36

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,33 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import BadgeItem from './BadgeItem';
 
 
+function useSearchBadges(badges) {
+    const [query, setQuery] = useState('');
+    const [filteredBadges, setFilteredBadges] = useState(badges);
 
-class BadgesList extends Component {
-    render() {
+    React.useMemo(() => {
+        const result = badges.filter(badge => {
+            return `${badge.firstName} ${badge.lastName}`
+                .toLowerCase()
+                .includes(query.toLowerCase());
+        });
 
-        if (this.props.badges.length === 0) {
-                return (
-                <div>
-                    <h3>No badges were found</h3>
-                    <Link className="btn btn-primary" to="/badges/new">
-                    Create new badge
-                    </Link>
-                </div>
-                );
+        setFilteredBadges(result);
+    }, [badges, query]);
+
+    return {
+        query,
+        setQuery,
+        filteredBadges
+    };
+}
+
+function BadgesList(props) {
+        const badges = props.badges;
+    
+        const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+    
+        if (filteredBadges.length === 0) {
+        return (
+            <div>
+            <div className="form-group">
+                <label>Filter Badges</label>
+                <input
+                type="text"
+                className="form-control"
+                value={query}
+                onChange={e => {
+                    setQuery(e.target.value);
+                }}
+                />
+            </div>
+    
+            <h3>No badges were found</h3>
+            <Link className="btn btn-primary" to="/badges/new">
+                Create new badge
+            </Link>
+            </div>
+        );
         }
+    
         return (
         <div className="BadgesList">
+            <div className="form-group">
+            <label>Filter Badges</label>
+            <input
+                type="text"
+                className="form-control"
+                value={query}
+                onChange={e => {
+                setQuery(e.target.value);
+                }}
+            />
+            </div>
+    
             <ul className="list-unstyled">
-            {this.props.badges.map(badge => {
+            {filteredBadges.map(badge => {
                 return (
                 <li key={badge.id}>
                     <Link
                     className="text-reset text-decoration-none"
-                    to={`/badges/${badge.id}`}>
-                        <BadgeItem badge={badge} />
+                    to={`/badges/${badge.id}`}
+                    >
+                    <BadgesListItem badge={badge} />
                     </Link>
                 </li>
                 );
@@ -35,7 +83,6 @@ class BadgesList extends Component {
             </ul>
         </div>
         );
-    }
 }
 
 export default BadgesList;
